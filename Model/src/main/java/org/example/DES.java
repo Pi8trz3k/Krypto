be1363cbc4ce7951b15be0e666a9d564a3423960
Pi8trz3k <242495@edu.p.lodz.pl>
2023-04-24 14:42:02 +0200

    Adding xorString method

--- Model/src/main/java/org/example/DES.java
+++ Model/src/main/java/org/example/DES.java
@@ -239,4 +239,18 @@ public class DES {
 
 
 
+    private static String xorString(String a, String b) {
+        StringBuilder xor = new StringBuilder("");
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) == b.charAt(i)) {
+                xor.append("0");
+            } else {
+                xor.append("1");
+            }
+        }
+        return xor.toString();
+    }
+
+
+
 }
